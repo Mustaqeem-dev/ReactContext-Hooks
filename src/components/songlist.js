9e63120f3335ed2d4f8676fb8ae1827dbebdd198
Mustaqeem-dev <mustaqeem.ahmed19@gmail.com>
2024-03-10 22:31:47 +0500

    Useeffect hook

--- src/components/songlist.js
+++ src/components/songlist.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import NewSong from "./newSong";
 
@@ -8,9 +8,16 @@ const Songlist = () => {
     { title: " phonks", id: 2 },
     { title: " sadsong ", id: 3 },
   ]);
+  const [age, setage] = useState(20);
   const addSong = (title) => {
     setsongs([...songs, { title, id: uuidv4() }]);
   };
+  useEffect(() => {
+    console.log("useeffect ran ", songs);
+  }, [songs]);
+  useEffect(() => {
+    console.log("useeffect ran ", age);
+  }, [age]);
   return (
     <div className="songlist">
       <ul>
@@ -19,6 +26,7 @@ const Songlist = () => {
         })}
       </ul>
       <NewSong addSong={addSong} />
+      <button onClick={() => setage(age + 1)}>ADD 1to age {age}</button>
     </div>
   );
 };
